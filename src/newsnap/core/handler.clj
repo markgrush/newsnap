2d616b7a2df43d410b8eca4978711efa1e4c1c9c
mark <markgrush303@gmail.com>
2014-12-13 13:57:30 -0600

    second commit

--- src/newsnap/core/handler.clj
+++ src/newsnap/core/handler.clj
@@ -1,11 +1,18 @@
 (ns newsnap.core.handler
   (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.adapter.jetty :as jetty]
+            [hiccup.page :refer [html5 include-css]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (handler/site app-routes))
+
+(defn -main []
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "5000"))]
+    (jetty/run-jetty app-routes {:port port})))
