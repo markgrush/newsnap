abbcbf081fd30cb05f68583d3e9dd9b98b99c584
mark <markgrush303@gmail.com>
2014-12-13 20:19:51 -0600

    split migration code from model to schema

--- src/newsnap/core/model.clj
+++ src/newsnap/core/model.clj
@@ -14,26 +14,6 @@
 (def spec (or (System/getenv "DATABASE_URL")
               (get-postgres-url)))
 
-(defn migrated? 
-  []
-  (-> (sql/query spec
-                 [(str "select count(*) from information_schema.tables "
-                       "where table_name='news'")])
-      first :count pos?))
-
-(defn migrate 
-  []
-  (when (not (migrated?))
-    (print "Creating database structure...") (flush)
-    (sql/db-do-commands spec
-                        (sql/create-table-ddl
-                         :news
-                         [:id :serial "PRIMARY KEY"]
-                         [:body :varchar "NOT NULL"]
-                         [:created_at :timestamp
-                          "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
-    (println " done")))
-
 (defn create
   [post]
   (when-not (clojure.string/blank? post)
---
+++ src/newsnap/core/schema.clj
@@ -0,0 +1,23 @@
+(ns newsnap.core.schema
+  (:require [clojure.java.jdbc :as sql]
+            [newsnap.core.model :as model]))
+
+(defn migrated? 
+  []
+  (-> (sql/query model/spec
+                 [(str "select count(*) from information_schema.tables "
+                       "where table_name='news'")])
+      first :count pos?))
+
+(defn migrate 
+  []
+  (when (not (migrated?))
+    (print "Creating database structure...") (flush)
+    (sql/db-do-commands model/spec
+                        (sql/create-table-ddl
+                         :news
+                         [:id :serial "PRIMARY KEY"]
+                         [:body :varchar "NOT NULL"]
+                         [:created_at :timestamp
+                          "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
+    (println " done")))
