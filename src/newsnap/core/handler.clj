9ca5c588a5fe3a810a38d278dc9bcb03b56711ab
mark <markgrush303@gmail.com>
2014-12-13 20:07:08 -0600

    added db migration code and basic table creation, also trying hiccup form

--- src/newsnap/core/handler.clj
+++ src/newsnap/core/handler.clj
@@ -3,6 +3,7 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
+            [hiccup.form :as form]
             [hiccup.page :refer [html5 include-css]]))
 
 (def title "Newsnap")
@@ -13,7 +14,7 @@
      [:head
       (include-css "simple.css")
       [:title title]]
-     [:body body]]))
+     [:body (form/text-area "text") body]]))
 
 (def some-text
   "Peanuts is a syndicated daily and Sunday American comic strip written and illustrated by Charles M. Schulz, which ran from October 2, 1950, to February 13, 2000, continuing in reruns afterward. The strip is the most popular and influential in the history of comic strips, with 17,897 strips published in all,[1] making it \"arguably the longest story ever told by one human being\".[2] At its peak, Peanuts ran in over 2,600 newspapers, with a readership of 355 million in 75 countries, and was translated into 21 languages.[3] It helped to cement the four-panel gag strip as the standard in the United States,[4] and together with its merchandise earned Schulz more than $1 billion.[1] Reprints of the strip are still syndicated and run in almost every U.S. newspaper.")
--- src/newsnap/core/model.clj
+++ src/newsnap/core/model.clj
@@ -1,4 +1,41 @@
 (ns newsnap.core.model
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as sql]
+            [ring.util.response :as ring]))
 
-;TODO: tons of shit
+; relative path of db url file
+(def postgresql-url-file "postgresql_url.txt")
+
+(defn get-postgres-url 
+  []
+  (-> (slurp postgresql-url-file)
+    (clojure.string/split #" ")
+    (second)))
+
+(def spec (or (System/getenv "DATABASE_URL")
+              (get-postgres-url)))
+
+(defn migrated? 
+  []
+  (-> (sql/query spec
+                 [(str "select count(*) from information_schema.tables "
+                       "where table_name='news'")])
+      first :count pos?))
+
+(defn migrate 
+  []
+  (when (not (migrated?))
+    (print "Creating database structure...") (flush)
+    (sql/db-do-commands shout/spec
+                        (sql/create-table-ddl
+                         :news
+                         [:id :serial "PRIMARY KEY"]
+                         [:body :varchar "NOT NULL"]
+                         [:created_at :timestamp
+                          "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
+    (println " done")))
+
+(defn create
+  [post]
+  (when-not (clojure.string/blank? post)
+    (sql/insert! spec :news [:body] post))
+  (ring/redirect "/"))
