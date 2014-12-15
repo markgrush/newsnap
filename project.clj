c8d6bf622f986047fbcd4c29bb9d0192f75bea0a
mark <markgrush303@gmail.com>
2014-12-14 20:17:46 -0600

    trying old forms with newest libs

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.6"]
-                 [ring "1.2.1"]
+                 [compojure "1.3.1"]
+                 [ring "1.3.2"]
                  [hiccup "1.0.5"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [org.postgresql/postgresql "9.3-1102-jdbc41"]]
