a11e90ae223ffec1630663bd7bbd2302dd153c61
mark <markgrush303@gmail.com>
2014-12-16 22:17:03 -0600

    changed thread to future

--- src/newsnap/core/model.clj
+++ src/newsnap/core/model.clj
@@ -35,5 +35,5 @@
           (not (clojure.string/blank? news)))
     (let [key (countdown-key)]
       (sql/insert! spec :news {:name name :email email :title title :body news :countdownkey key})
-      (.start (Thread. (countdown title news key)))))
+      (future (countdown title news key))))
   (ring/redirect "/"))
