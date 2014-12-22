893c0065064ce6502a5d4cdedbf99c088bdda20b
mark <markgrush303@gmail.com>
2014-12-21 22:52:57 -0600

    added anti-forgery field in forms to allow post requests to be procesed securely, as now we have an anti-forgery wrapper

--- src/newsnap/core/handler.clj
+++ src/newsnap/core/handler.clj
@@ -3,6 +3,7 @@
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
             [hiccup.form :as form]
             [hiccup.page :refer [html5 include-css]]
             [hiccup.util :refer [escape-html]]
@@ -15,20 +16,29 @@
 (def form-test
   [:div {:class "primary form"}
      (form/form-to [:post "/"]
-                [:div [:label {:class "in-form" :for "op-name"} "Name:"]]
-                [:div [:input {:class "in-form" :type "text" :id "op-name" :name "op-name"}]]
-                [:div [:label {:class "in-form" :for "op-email"} "Email:"]]
-                [:div [:input {:class "in-form" :type "op-email" :id "op-email" :name "op-email"}]]
-                [:div [:label {:class "in-form" :for "title"} "Title:"]]
-                [:div [:textarea {:class "in-form" :id "title" :name "title" :rows "1" :cols "50"}]]
-                [:div [:label {:class "in-form" :for "news"} "News:"]]
-                [:div [:textarea {:class "in-form" :id "news" :name "news" :rows "10" :cols "50"}]]
-                [:div [:input {:class "in-form primary-light btn" :type "submit" :value "submit"}]])])
+                   ;; MUST add this func to each form to prevent 
+                   ;; "Invalid anti-forgery token" message since the 
+                   ;; ring-defaults site-defaults wrappers has a 
+                   ;; anti-forgery wrapper. read more here:
+                   ;; https://github.com/ring-clojure/ring-anti-forgery
+                   ;; good explanation for what's a CSRF attack:
+                   ;; http://www.lispcast.com/clojure-web-security
+                   (anti-forgery-field)
+                   [:div [:label {:class "in-form" :for "op-name"} "Name:"]]
+                   [:div [:input {:class "in-form" :type "text" :id "op-name" :name "op-name"}]]
+                   [:div [:label {:class "in-form" :for "op-email"} "Email:"]]
+                   [:div [:input {:class "in-form" :type "op-email" :id "op-email" :name "op-email"}]]
+                   [:div [:label {:class "in-form" :for "title"} "Title:"]]
+                   [:div [:textarea {:class "in-form" :id "title" :name "title" :rows "1" :cols "50"}]]
+                   [:div [:label {:class "in-form" :for "news"} "News:"]]
+                   [:div [:textarea {:class "in-form" :id "news" :name "news" :rows "10" :cols "50"}]]
+                   [:div [:input {:class "in-form primary-light btn" :type "submit" :value "submit"}]])])
 
 (defn reply-form
   [route]
   [:div {:class "primary form"}
    (form/form-to [:post route]
+                 (anti-forgery-field)
                  [:div [:label {:class "in-form" :for "replier-name"} "Name:"]]
                  [:div [:input {:class "in-form" :type "text" :id "replier-name" :name "replier-name"}]]
                  [:div [:label {:class "in-form" :for "replier-email"} "Email:"]]
@@ -105,7 +115,6 @@
     ;; when "app is sitting behind a load balancer or reverse proxy, 
     ;; as is often the case in cloud-based deployments"
     (wrap-defaults (assoc site-defaults :proxy true))))
-    
 
 (defn start [port]
   (jetty/run-jetty app {:port port :join? false}))
