0e3ed6b49ad12781831ea65984482844519d1b96
mark <markgrush303@gmail.com>
2015-01-20 21:55:36 -0600

    take me away

--- src/newsnap/core/handler.clj
+++ src/newsnap/core/handler.clj
@@ -109,7 +109,7 @@
     (let [content-type (get-in ctx [:representation :media-type])]
       (condp = content-type
         "text/html" (root (reply-form (str "/" thread)) (news-post thread))
-        "application/json" (json/write-str (into [] (model/news-item thread)))
+        "application/json" (json/write-str (first (model/news-item thread)))
         {:message "You requested a media type"
          :media-type content-type}))))
       
